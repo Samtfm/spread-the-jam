06a254cb001c379e9e24ea48ff316ca1255ee76c
Samtfm <sfmemail@gmail.com>
2017-05-16 10:03:11 -0700

    write session actions

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -3,10 +3,30 @@ import * as APIUtil from '../util/session_api_util';
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
 export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
 
+export const receiveCurrentUser = user => ({
+  type: RECEIVE_CURRENT_USER,
+  user
+});
+
+export const receiveErrors = errors => ({
+  type: RECEIVE_ERRORS,
+  errors
+});
+
+export const signUp = user => dispatch => (
+  APIUtil.signUp(user)
+    .then(res => dispatch(receiveCurrentUser(res)),
+          err => dispatch(receiveErrors(err)))
+);
+
 export const signIn = user => dispatch => (
   APIUtil.signIn(user)
-    .then( res => dispatch(receiveCurrentUser(res),
-            err => dispatch(receiveErrors(err))))
-)
+    .then(res => dispatch(receiveCurrentUser(res)),
+          err => dispatch(receiveErrors(err)))
+);
 
-export const signOut = () => dispatch +
+export const signOut = () => dispatch => (
+  APIUtil.signOut()
+    .then(res => dispatch(receiveCurrentUser(null)),
+          err => dispatch(receiveErrors(err)))
+);
