14cfd42c6e2520e61655705471700a4d00f0a759
Samtfm <sfmemail@gmail.com>
2017-05-15 19:21:30 -0700

    create auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  namespace :api do
-    get 'users_controller/create'
+  namespace :api, defaults: { format: :json } do
+    resource :session, only: [:create, :destroy]
+    resources :users, only: [:create]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
