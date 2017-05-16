fdc5d7f4be07a12324d80341c061f573e91ea770
Samtfm <sfmemail@gmail.com>
2017-05-16 10:46:04 -0700

    session reducer

---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,16 @@
+import { RECEIVE_CURRENT_USER, RECEIVE_ERRORS }
+  from '../actions/session_actions';
+import merge from 'lodash/merge';
+const SessionReducer = (state = {}, action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      return merge({}, state, { currentUser: action.user });
+    case RECEIVE_ERRORS:
+      return merge({}, state, { errors: action.errors });
+    default:
+      return state;
+  }
+};
+
+export default SessionReducer;
