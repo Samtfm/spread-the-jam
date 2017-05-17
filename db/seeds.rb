024fc3756e346045f8b28df5b893ee41597918e4
Samtfm <sfmemail@gmail.com>
2017-05-16 23:12:20 -0700

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(username: sam, password: 'password')
+User.create(username: "sam", password: 'password')
+User.create(username: "scrappy", password: 'password')
