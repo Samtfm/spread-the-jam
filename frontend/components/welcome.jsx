e080f7430b24a333c8b2a630c36d1e8673af99ee
Samtfm <sfmemail@gmail.com>
2017-05-16 19:25:37 -0700

    better link buttons

--- frontend/components/welcome.jsx
+++ frontend/components/welcome.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 
 const Welcome = () => (
   <div className='welcome'>
-    <button>
-      <Link to='/signin' className='button'>Log In</Link>
-    </button>
-    <button>
-      <Link to='/signup' className='button'>Sign Up</Link>
-    </button>
+    <Link to='/signin' className='button'>
+      <button>Log In</button>
+    </Link>
+    <Link to='/signup' className='button'>
+      <button>Sign Up</button>
+    </Link>
   </div>
 );
 
