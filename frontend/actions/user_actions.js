0db9f60f77da33d4d2a8e4d9bacb295641d81c70
Samtfm <sfmemail@gmail.com>
2017-05-17 17:47:43 -0700

    write api to update user

---
+++ frontend/actions/user_actions.js
@@ -0,0 +1,9 @@
+import * as APIUtil from '../util/user_api_util';
+
+
+
+// export const update = user => dispatch => (
+//   APIUtil.update(user)
+//     .then(res => dispatch(receiveCurrentUser(res)),
+//           err => dispatch(receiveErrors(err.responseJSON)))
+// );
--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -1,7 +1,7 @@
 import { RECEIVE_CURRENT_USER, RECEIVE_ERRORS }
   from '../actions/session_actions';
 import merge from 'lodash/merge';
-const SessionReducer = (state = { currentUser: null, errors: [] }, action) => {
+const SessionReducer = (state = { currentUser: null }, action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
