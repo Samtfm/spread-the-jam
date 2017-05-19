dea52aea82244fa7015c02964fc71d97e4600771
Samtfm <sfmemail@gmail.com>
2017-05-19 11:47:35 -0700

    seed events

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(username: "sam", password: 'password')
-User.create(username: "scrappy", password: 'password')
+sam = User.create(username: "sam", password: 'password')
+scrappy = User.create(username: "scrappy", password: 'password')
+frank = User.create(username: "Frank", password: 'password')
 
 City.destroy_all
-City.create(name: "San Francisco", img_url: 'http://res.cloudinary.com/samtfm/image/upload/c_scale,w_1200/v1495168184/san_francisco.jpg' )
-City.create(name: "New York", img_url: 'http://res.cloudinary.com/samtfm/image/upload/c_scale,w_1200/v1495168184/new_york.jpg')
+sf = City.create(name: "San Francisco", img_url: 'http://res.cloudinary.com/samtfm/image/upload/c_scale,w_1200/v1495168184/san_francisco.jpg' )
+ny = City.create(name: "New York", img_url: 'http://res.cloudinary.com/samtfm/image/upload/c_scale,w_1200/v1495168184/new_york.jpg')
 City.create(name: "Nashville", img_url: 'http://res.cloudinary.com/samtfm/image/upload/c_scale,w_1200/v1495168184/nashville.jpg')
 City.create(name: "Chicago", img_url: 'http://res.cloudinary.com/samtfm/image/upload/c_scale,w_1200/v1495168184/chicago.jpg')
 City.create(name: "London", img_url: 'http://res.cloudinary.com/samtfm/image/upload/c_scale,w_1200/v1495168184/london.jpg')
 City.create(name: "New Orleans", img_url: 'http://res.cloudinary.com/samtfm/image/upload/c_scale,w_1200/v1495168184/new_orleans.jpg')
+
+event1 Event.create(
+  address: "160 spear street",
+  description: "neat folks and rad tunes!",
+  city_id: sf.id,
+  host_id: sam.id,
+  date_time: DateTime.new(2017, 5, 28, 17, 30, 0)
+);
+
+event2 Event.create(
+  address: "160 spear street",
+  description: "lunch time jam!",
+  city_id: sf.id,
+  host_id: sam.id,
+  date_time: DateTime.new(2017, 5, 29, 12, 30, 0)
+);
+
+event3 Event.create(
+  address: "new york, new york",
+  description: "some folks like to get away",
+  city_id: sf.id,
+  host_id: frank.id,
+  date_time: DateTime.new(2017, 6, 15, 14, 00, 0)
+);
