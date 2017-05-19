c9b786bc43d9a8214ef2887f0b369dd93d3db97f
Samtfm <sfmemail@gmail.com>
2017-05-19 11:17:05 -0700

    write events reducer

---
+++ frontend/reducers/events_reducer.js
@@ -0,0 +1,15 @@
+import { RECEIVE_EVENTS } from '../actions/event_actions';
+import merge from 'lodash/merge';
+
+const EventsReducer = (state = {}, action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_EVENTS:
+      return merge({}, state, action.events);
+    default:
+      return state;
+  }
+};
+
+
+export default EventsReducer;
