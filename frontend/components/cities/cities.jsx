b77a83d91c65d3d2e009b09640b46f2c1d453622
Samtfm <sfmemail@gmail.com>
2017-05-22 23:22:32 -0700

    nice, there is less color now

--- frontend/components/cities/cities.jsx
+++ frontend/components/cities/cities.jsx
@@ -10,15 +10,17 @@ class Cities extends React.Component{
   render(){
     return (
       <section className="cities">
-        <div className="banner">
+
+        {/*<div className="banner">
           <div className="banner-image"
             style={{backgroundImage: `url(${'https://linkablenetworks.com/wp-content/themes/linkablenetworks/library/images/svg/logo-mark.svg'})`}}>hi</div>
           <div className='banner-content'>
             <h1>{'where are ya?'}</h1>
           </div>
-        </div>
+        </div>*/}
+
         <ul>
-          {this.props.cities.slice(0,3).map(city => (
+          {this.props.cities.map(city => (
             <Link key={city.id} tabIndex="-1" to={`/cities/${city.id}`}>
               <li style={{backgroundImage: `url(${city.imgUrl})`}}>
                 <button>
@@ -26,17 +28,6 @@ class Cities extends React.Component{
                 </button>
               </li>
             </Link>
-          ))}
-        </ul>
-        <ul>
-          {this.props.cities.slice(3,6).map(city => (
-          <Link key={city.id} tabIndex="-1" to={`/cities/${city.id}`}>
-            <li style={{backgroundImage: `url(${city.imgUrl})`}}>
-              <button>
-                {city.name}
-              </button>
-            </li>
-          </Link>
           ))}
         </ul>
       </section>
