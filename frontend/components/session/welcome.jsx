2f19f48131ff694fcab815822db3f9b6eb7daa9b
Samtfm <sfmemail@gmail.com>
2017-05-23 10:08:11 -0700

    better welcome

--- frontend/components/session/welcome.jsx
+++ frontend/components/session/welcome.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Welcome = () => (
-  <div className='welcome'>
-    <h2>Welcome!</h2>
-    <nav>
-      <Link tabIndex="-1" to='/signin'>
-        <button>Log In</button>
-      </Link>
-      <Link tabIndex="-1" to='/signup'>
-        <button>Sign Up</button>
-      </Link>
-    </nav>
-  </div>
+    <div className="banner">
+      <div className="banner-image"
+        style={{backgroundImage: `url(${'http://res.cloudinary.com/samtfm/image/upload/v1495558030/guitar_notebook.jpg'})`}}></div>
+      <div className='banner-content'>
+        <h2>Let's play music together!</h2>
+        <nav>
+          <Link tabIndex="-1" to='/signup'>
+            <button>Sign Up</button>
+          </Link>
+        </nav>
+      </div>
+    </div>
+
 );
 
 
