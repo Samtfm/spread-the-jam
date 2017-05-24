f0d3cf123206ab3174190f397bc3b14d66931fc8
Samtfm <sfmemail@gmail.com>
2017-05-23 21:14:55 -0700

    front page content

--- frontend/components/session/welcome.jsx
+++ frontend/components/session/welcome.jsx
@@ -2,19 +2,36 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Welcome = () => (
-    <div className="banner welcome">
-      <div className="banner-image"
-        style={{backgroundImage: `url(${'http://res.cloudinary.com/samtfm/image/upload/v1495558030/guitar_notebook.jpg'})`}}></div>
-      <div className='banner-content'>
-        <h2>Let's play music together!</h2>
-        <nav>
-          <Link tabIndex="-1" to='/signup'>
-            <button>Sign Up</button>
-          </Link>
-        </nav>
-      </div>
-    </div>
+    <div>
 
+      <div className="banner welcome">
+        <div className="banner-image"
+          style={{backgroundImage: `url(${'http://res.cloudinary.com/samtfm/image/upload/v1495558030/guitar_notebook.jpg'})`}}></div>
+        <div className='banner-content'>
+          <h2>Let's play music together!</h2>
+          <nav>
+            <Link tabIndex="-1" to='/signup'>
+              <button>Sign Up</button>
+            </Link>
+          </nav>
+        </div>
+      </div>
+      <section className='steps'>
+        <div>
+          <h2>Show up for a jam</h2>
+          <p>You and a few others meet up with your instruments.</p>
+        </div>
+        <div>
+          <h2>Create live music</h2>
+          <p>Put your sounds together in whatever ways they fit.</p>
+        </div>
+        <div>
+          <h2>See where it takes you</h2>
+          <p>Explore new styles! Start a band even!</p>
+        </div>
+      </section>
+
+    </div>
 );
 
 
